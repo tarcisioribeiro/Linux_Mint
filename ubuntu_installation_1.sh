ce7369e5391ea0927179ed25258c3b168a9a70b4
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2025-01-06 19:35:29 -0300

    Enviando.

---
+++ ubuntu_installation_1.sh
@@ -0,0 +1,159 @@
+#!/usr/bin/bash
+red() {
+    echo -e "\033[31m$1\033[0m"
+}
+green() {
+    echo -e "\033[32m$1\033[0m"
+}
+
+blue() {
+    echo -e "\033[34m$1\033[0m"
+}
+
+cd ~
+mkdir repos
+mkdir scripts
+
+red "\n |--------- Instalação - Parte 1 ---------| \n"
+
+sleep 10
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install nala
+
+blue "\nInstalando o Nala.\n"
+sleep 3
+
+sudo nala install \
+  build-essential \
+  git \
+  neofetch \
+  curl \
+  tmux \
+  wget \
+  net-tools \
+  btop \
+  tilix \
+  flatpak \
+  python3-venv \
+  python3-tk \
+  python3-pip \
+  python3.10-full \
+  python3.10-dev \
+  dkms \
+  perl \
+  gcc \
+  make \
+  default-libmysqlclient-dev \
+  libssl-dev \
+  zlib1g-dev \
+  libbz2-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  libncurses5-dev \
+  libncursesw5-dev \
+  llvm \
+  xz-utils \
+  tk-dev \
+  libffi-dev \
+  liblzma-dev \
+  python3-openssl \
+  timeshift \
+  bat \
+  gdebi \
+  ruby-full \
+  nodejs \
+  npm \
+  fzf \
+  gnome-tweaks \
+  gnome-shell-extension-manager \
+  mpv \
+  feh \
+  mysql-server \
+  openssh-server
+
+sudo nala install zsh -y
+chsh -s /usr/bin/zsh
+zsh
+
+blue "\nInstalando o Oh My ZSH...\n"
+sleep 3
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+blue "\nInstalando o Oh My Posh...\n"
+sleep 3
+
+cd ~
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.omp.*
+rm ~/.poshthemes/themes.zip
+
+cd ~
+mkdir -p ~/repos
+mkdir -p ~/.icons
+mkdir -p ~/.themes
+mkdir -p ~/scripts
+cd ~/repos
+git clone https://github.com/tarcisioribeiro/ExpenseLit.git
+git clone https://github.com/tarcisioribeiro/Password_Manager.git
+
+mkdir -p ~/.config/autostart
+
+cd ~
+cp repos/Terminal/customization/zsh/tj-dracula.omp.json ~/.poshthemes
+cp repos/Terminal/customization/bash/logo-ls_Linux_x86_64.tar.gz ~/Downloads
+cd ~/Downloads
+tar -zxf logo-ls_Linux_x86_64.tar.gz
+cd ~/Downloads/logo-ls_Linux_x86_64
+sudo cp logo-ls /usr/local/bin
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+sudo nala remove libmagickcore-6.q16-6 imagemagick-6.q16 imagemagick-6-common imagemagick idle-python3.10
+
+blue "\nInstalando o Neovim...\n"
+sleep 3
+
+cd ~/Downloads
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
+
+blue "\nInstalando o NVM...\n"
+sleep 3
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+blue "\nInstalando o HomeBrew...\n"
+sleep 3
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+blue "\nInstalando o Oh My Bash...\n"
+sleep 3
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+blue "\nInstalando o Starship...\n"
+sleep 3
+curl -sS https://starship.rs/install.sh | sh
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+cd ~
+cp repos/Terminal/customization/zsh/.zshrc ~
+cp repos/Terminal/customization/zsh/.zsh_aliases ~
+cp repos/Terminal/customization/bash/.bashrc ~
+cp repos/Terminal/customization/bash/.bash_aliases ~
+cp repos/Terminal/customization/git/.gitconfig ~
+cp repos/Terminal/customization/tmux/.tmux.conf ~
+cp repos/Terminal/customization/starship/starship.toml ~/.config
+
+green "\nConcluído. Reinicie o terminal e execute o próximo instalador.\n"
+sleep 3
---
+++ ubuntu_installation_2.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/bash
+red() {
+    echo -e "\033[31m$1\033[0m"
+}
+green() {
+    echo -e "\033[32m$1\033[0m"
+}
+
+blue() {
+    echo -e "\033[34m$1\033[0m"
+}
+
+brew install eza glow tldr fd git-delta
+nvm install 20.17.0
+
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+mv ~/.config/nvim ~/.config/nvim_old
+cp -r ~/repos/Terminal/customization/nvim ~/.config
+
+mkdir -p ~/.config/tilix/schemes/
+wget  -qO $HOME"/.config/tilix/schemes/dracula.json" https://git.io/v7QaT
+
+sleep 3
+
+nvim
+
+cd ~/Downloads
+
+blue "\nInstalando pacotes .deb...\n"
+sleep 3
+
+cp ~/repos/Ubuntu/packages/*.deb .
+sudo gdebi code.deb
+sudo gdebi google-chrome.deb
+sudo gdebi mysql-workbench.deb
+sudo gdebi virtualbox.deb
+
+blue "\nInstalando o i3wm...\n"
+
+sleep 3
+
+sudo apt install -y build-essential libxcb1-dev libxcb-keysyms1-dev \
+libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev \
+libxcb-randr0-dev libxcb-cursor0-dev libxcb-xinerama0-dev \
+libpango1.0-dev libx11-dev libxrandr-dev libxinerama-dev \
+libxss-dev libglib2.0-dev libev-dev
+
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev \
+libxcb-util0-dev libxcb-icccm4-dev libyajl-dev \
+libstartup-notification0-dev libxcb-randr0-dev \
+libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
+autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
+
+/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2024.03.04_all.deb keyring.deb SHA256:f9bb4340b5ce0ded29b7e014ee9ce788006e9bbfe31e96c09b2118ab91fca734
+sudo apt install ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+sudo apt update
+
+sudo apt install i3
+
+blue "\nInstalando o i3lock-color...\n"
+sleep 3
+sudo apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
+cd ~/Downloads
+git clone https://github.com/Raymo111/i3lock-color.git
+cd i3lock-color
+./build.sh
+./install-i3lock-color.sh
+
+cp ~/repos/Ubuntu/scripts/lock ~/scripts/
+
+blue "\nInstalando gaps do i3...\n"
+
+cd ~/Downloads
+git clone https://www.github.com/jbenden/i3-gaps-rounded i3-gaps
+cd i3-gaps
+mkdir -p build && cd build
+meson ..
+ninja
+sudo ninja install
+
+sudo mv /usr/share/mysql-workbench/data/code_editor.xml /usr/share/mysql-workbench/data/code_editor_old.xml
+sudo cp ~/repos/Ubuntu/mysql/code_editor.xml /usr/share/mysql-workbench/
+
+sudo nala install cmatrix pavucontrol

c6386a0c718829864b02f862bbacd8bf5f2ed8f0
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2025-06-25 22:19:20 -0300

    Corrigindo.

--- packages/development-tools/flutter.sh
+++ packages/development-tools/flutter.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-mkdir -p ~/development
+mkdir -p ~/Development
 cd ~/Downloads
 wget -q https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.27.1-stable.tar.xz
-tar -xf ~/Downloads/flutter_linux_3.27.1-stable.tar.xz -C ~/development/
+tar -xf ~/Downloads/flutter_linux_3.27.1-stable.tar.xz -C ~/Development/
 rm flutter_linux_3.27.1-stable.tar.xz

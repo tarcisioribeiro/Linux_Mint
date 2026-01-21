9acbb1f08d34aa4fabc83c5b89a3312c60070121
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2026-01-21 11:46:04 -0300

    Updating README.

--- scripts/installer/main.py
+++ scripts/installer/main.py
@@ -8,6 +8,15 @@ class Installer:
     def __init__(self):
         pass
 
+    def open_firefox(self):
+        sleep(1)
+        gui.press("win")
+        sleep(1)
+        gui.write("Firefox")
+        sleep(1)
+        gui.press("enter")
+        sleep(1)
+
     def open_chrome(self):
         sleep(1)
         gui.press("win")
@@ -33,67 +42,79 @@ class Installer:
         sleep(1)
         gui.hotkey("ctrl", "t")
 
+    def install_chrome_styles(self):
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fgithub%2Fmaster%2Fstyle.user.css",
+            x_position=74,
+            y_position=193
+        )
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fyoutube%2Fmain%2Fdracula.user.css",
+            x_position=77,
+            y_position=210
+        )
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fchatgpt%2Fmain%2Fchatgpt-dracula.user.css",
+            x_position=77,
+            y_position=210
+        )
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fgitlab%2Fmaster%2Fdracula.user.css",
+            x_position=74,
+            y_position=210
+        )
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fuserstyles.world%2Fapi%2Fstyle%2F24931.user.css",
+            x_position=68,
+            y_position=205
+        )
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fstackoverflow%2Fmaster%2Fdracula_for_stackoverflow.user.css",
+            x_position=73,
+            y_position=207
+        )
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fgoogle-calendar%2Fmain%2Fgcal-dracula.user.css",
+            x_position=63,
+            y_position=213
+        )
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fduolingo%2Fmain%2Fdracula-duolingo.user.css",
+            x_position=65,
+            y_position=215
+        )
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fuserstyles.world%2Fapi%2Fstyle%2F9946.user.css",
+            x_position=70,
+            y_position=209
+        )
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fgoogle-search%2Fmain%2Fgoogle-search.user.css",
+            x_position=72,
+            y_position=209
+        )
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Flichess%2Fmain%2Fdracula-lichess.user.css",
+            x_position=68,
+            y_position=209
+        )
+        self.install_css_styles(
+            style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fcodepen%2Fmain%2Fstyle.user.styl",
+            x_position=66,
+            y_position=191
+        )
+
+    def install_firefox_styles(self):
+        self.install_css_styles(
+            style_url="moz-extension://54113f89-4029-4a47-b729-f71fd3925782/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fyoutube%2Fmain%2Fdracula.user.css",
+            x_position=69,
+            y_position=214
+        )
+
 
 if __name__ == "__main__":
     installer = Installer()
     installer.open_chrome()
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fgithub%2Fmaster%2Fstyle.user.css",
-        x_position=74,
-        y_position=193
-    )
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fyoutube%2Fmain%2Fdracula.user.css",
-        x_position=77,
-        y_position=210
-    )
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fchatgpt%2Fmain%2Fchatgpt-dracula.user.css",
-        x_position=77,
-        y_position=210
-    )
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fgitlab%2Fmaster%2Fdracula.user.css",
-        x_position=74,
-        y_position=210
-    )
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fuserstyles.world%2Fapi%2Fstyle%2F24931.user.css",
-        x_position=68,
-        y_position=205
-    )
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fstackoverflow%2Fmaster%2Fdracula_for_stackoverflow.user.css",
-        x_position=73,
-        y_position=207
-    )
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fgoogle-calendar%2Fmain%2Fgcal-dracula.user.css",
-        x_position=63,
-        y_position=213
-    )
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fduolingo%2Fmain%2Fdracula-duolingo.user.css",
-        x_position=65,
-        y_position=215
-    )
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fuserstyles.world%2Fapi%2Fstyle%2F9946.user.css",
-        x_position=70,
-        y_position=209
-    )
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fgoogle-search%2Fmain%2Fgoogle-search.user.css",
-        x_position=72,
-        y_position=209
-    )
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Flichess%2Fmain%2Fdracula-lichess.user.css",
-        x_position=68,
-        y_position=209
-    )
-    installer.install_css_styles(
-        style_url="chrome-extension://clngdbkpkpeebahjckkjfobafhncgmne/install-usercss.html?updateUrl=https%3A%2F%2Fraw.githubusercontent.com%2Fdracula%2Fcodepen%2Fmain%2Fstyle.user.styl",
-        x_position=66,
-        y_position=191
-    )
+    installer.install_chrome_styles()
+    # installer.open_firefox()
+    # installer.install_firefox_styles()

766a2c81d1fd1a0bd5d764d1f2335dd4999b8d81
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2026-01-23 15:33:34 -0300

    Updating app.

--- installation_3.sh
+++ installation_3.sh
@@ -19,6 +19,33 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+  curl \
+  gnupg2
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg &&
+  curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list |
+  sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' |
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+
+export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.18.1-1
+sudo apt-get install -y \
+  nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+  nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+  libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+  libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
+
+docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+docker exec -it ollama ollama run llama2
+
+cd /media/tarcisio/Seagate/Linux/Customization/grub/
+sudo mkdir -p /boot/grub/themes/
+sudo cp -r dracula /boot/grub/themes/
+sudo cp grub /etc/default/
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
 sleep 2
 
 cd $HOME/Development/Linux_Mint/scripts/installer
--- scripts/monitor_setup.sh
+++ scripts/monitor_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# xrandr --output HDMI-0 --primary --mode 2560x1080 --rate 75 --pos 0x0 --rotate normal \
-#   --output eDP-1-1 --mode 1920x1080 --rate 60 --pos 2560x0 --rotate normal
+xrandr --output HDMI-0 --primary --mode 2560x1080 --rate 75 --pos 0x0 --rotate normal \
+  --output eDP-1-1 --mode 1920x1080 --rate 60 --pos 2560x0 --rotate normal
 
-# i3-msg "workspace 1; move workspace to output HDMI-0"
-# i3-msg "workspace 2; move workspace to output eDP-1-1"
-# i3-msg "workspace 3; move workspace to output HDMI-0"
-# i3-msg "workspace 4; move workspace to output eDP-1-1"
+i3-msg "workspace 1; move workspace to output HDMI-0"
+i3-msg "workspace 2; move workspace to output eDP-1-1"
+i3-msg "workspace 3; move workspace to output HDMI-0"
+i3-msg "workspace 4; move workspace to output eDP-1-1"
 
 xrandr --output DP-1-1-3 --off \
   --output HDMI-0 --primary --mode 2560x1080 --rate 75 --pos 0x0 --rotate normal \
